ca67888e3880d4b8893143ab8d81d7adebb88b05
Abubakr <realchampiontheno1@gmail.com>
2021-01-28 17:00:11 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,34 +1,44 @@
+const addButton = document.querySelector('#addBtn');
 
-const addButton = document.querySelector('#add');
+addButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  addNewNote();
+});
 
-var titleText = document.getElementById('title').value;
-var noteText = document.getElementById('note').value;
-var checkTrue = document.getElementById('completedtrue').checked;
-var checkFalse = document.getElementById('completedfalse').checked;
-var priorityLow= document.getElementById('prioritylow').checked;
-var prirotyHigh = document.getElementById('priorityhigh').checked;
+const addNewNote = (text = '') => {
+  const titleText = document.getElementById('title').value;
+  const noteText = document.getElementById('note').value;
+  const checkTrue = document.getElementById('completedtrue').checked;
+  const checkFalse = document.getElementById('completedfalse').checked;
+  const priorityLow = document.getElementById('prioritylow').checked;
+  const prirotyHigh = document.getElementById('priorityhigh').checked;
 
-const addNewNote =  (Text = '') => {
-const note = document.createElement('div');
-note.classList.add('note');
-const htmlData = `
-<div class="row todo">
-<div class="col-2">
-    <h4>${titleText}</h4>
-</div>
-<div class="col-1"><h4></h4></div>
-<div class="col-2"><h4></h4></div>
-<div class="col-3">
-    <h4></h4>
-</div>
-<div class="col-4">
-    <h4>${noteText}</h4>
-</div>
-</div>`;
-note.insertAdjacentHTML('afterbegin' , htmlData);
-console.log(note);
-document.body.appendChild(note);
+  const note = document.createElement('div');
+  note.classList.add('note');
 
+  const htmlData = `
+        <div class="row todo">
+            <div class="col-2">
+                <h4>${titleText}</h4>
+            </div>
+            <div class="col-1">
+                <h4></h4>
+            </div>
+            <div class="col-2">
+                <h4></h4>
+            </div>
+            <div class="col-3">
+                <h4></h4>
+            </div>
+            <div class="col-4">
+                <h4>${noteText}</h4>
+            </div>
+        </div>
+    `;
 
+  note.insertAdjacentHTML('afterbegin', htmlData);
+
+  console.log(note);
+
+  document.body.appendChild(note);
 };
-addButton.addEventListener('click',()=> addNewNote() );
